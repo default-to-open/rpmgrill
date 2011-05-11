c3b81387b82f1d1d02aa009aa6ce5a7bf6814b48
Ed Santiago <santiago@redhat.com>
2011-05-11 10:40:16 -0600

    checkpoint; include group checks, and homedir/shell/uid checks

--- lib/RPM/Grill/Plugin/RpmScripts.pm
+++ lib/RPM/Grill/Plugin/RpmScripts.pm
@@ -35,56 +35,75 @@ sub doc {
 FIXME FIXME FIXME
 END_DOC
 
-
-# This is a direct copy-paste from shadow-utils-4.1.4.2-9.el6
-# This is a direct copy-and-paste from shadow-4.1.4.2/src/useradd.c .
-# FIXME: handle earlier versions of useradd?
-our $Useradd_Opts = <<'END_USERADD_OPTS';
-		static struct option long_options[] = {
-			{"base-dir", required_argument, NULL, 'b'},
-			{"comment", required_argument, NULL, 'c'},
-			{"home-dir", required_argument, NULL, 'd'},
-			{"defaults", no_argument, NULL, 'D'},
-			{"expiredate", required_argument, NULL, 'e'},
-			{"inactive", required_argument, NULL, 'f'},
-			{"gid", required_argument, NULL, 'g'},
-			{"groups", required_argument, NULL, 'G'},
-			{"help", no_argument, NULL, 'h'},
-			{"skel", required_argument, NULL, 'k'},
-			{"key", required_argument, NULL, 'K'},
-			{"create-home", no_argument, NULL, 'm'},
-			{"no-create-home", no_argument, NULL, 'M'},
-			{"no-log-init", no_argument, NULL, 'l'},
-			{"no-user-group", no_argument, NULL, 'N'},
-			{"non-unique", no_argument, NULL, 'o'},
-			{"password", required_argument, NULL, 'p'},
-			{"system", no_argument, NULL, 'r'},
-			{"shell", required_argument, NULL, 's'},
+#
+# Command-line options for the useradd and groupadd commands.
+#
+our %Command_Line_Options;
+#
+# This is a direct copy-paste from shadow-utils-4.1.4.2-9.el6 /src/useradd.c
+#
+$Command_Line_Options{useradd} = <<'END_USERADD_OPTS';
+                static struct option long_options[] = {
+                        {"base-dir", required_argument, NULL, 'b'},
+                        {"comment", required_argument, NULL, 'c'},
+                        {"home-dir", required_argument, NULL, 'd'},
+                        {"defaults", no_argument, NULL, 'D'},
+                        {"expiredate", required_argument, NULL, 'e'},
+                        {"inactive", required_argument, NULL, 'f'},
+                        {"gid", required_argument, NULL, 'g'},
+                        {"groups", required_argument, NULL, 'G'},
+                        {"help", no_argument, NULL, 'h'},
+                        {"skel", required_argument, NULL, 'k'},
+                        {"key", required_argument, NULL, 'K'},
+                        {"create-home", no_argument, NULL, 'm'},
+                        {"no-create-home", no_argument, NULL, 'M'},
+                        {"no-log-init", no_argument, NULL, 'l'},
+                        {"no-user-group", no_argument, NULL, 'N'},
+                        {"non-unique", no_argument, NULL, 'o'},
+                        {"password", required_argument, NULL, 'p'},
+                        {"system", no_argument, NULL, 'r'},
+                        {"shell", required_argument, NULL, 's'},
 #ifdef WITH_SELINUX
-			{"selinux-user", required_argument, NULL, 'Z'},
+                        {"selinux-user", required_argument, NULL, 'Z'},
 #endif
-			{"uid", required_argument, NULL, 'u'},
-			{"user-group", no_argument, NULL, 'U'},
-			{NULL, 0, NULL, '\0'}
+                        {"uid", required_argument, NULL, 'u'},
+                        {"user-group", no_argument, NULL, 'U'},
+                        {NULL, 0, NULL, '\0'}
 END_USERADD_OPTS
+#
+# This is a direct copy-paste from shadow-utils-4.1.4.2-9.el6 /src/groupadd.c
+#
+$Command_Line_Options{groupadd} = <<'END_GROUPADD_OPTS';
+        static struct option long_options[] = {
+                {"force", no_argument, NULL, 'f'},
+                {"gid", required_argument, NULL, 'g'},
+                {"help", no_argument, NULL, 'h'},
+                {"key", required_argument, NULL, 'K'},
+                {"non-unique", no_argument, NULL, 'o'},
+                {"password", required_argument, NULL, 'p'},
+                {"system", no_argument, NULL, 'r'},
+                {NULL, 0, NULL, '\0'}
+END_GROUPADD_OPTS
 
 # Convert that
-my @Useradd_Opts;
-USERADD_OPT:
-for my $line ( split "\n", $Useradd_Opts ) {
-    $line =~ m|^\s+ \{\s* "(.*?)",\s* (.*?)_argument,\s* .*?,\s* '(.)'\s*\}|x
-        or next USERADD_OPT;
-    my ( $long, $type, $short ) = ( $1, $2, $3 );
-    $type eq 'required' || $type eq 'no'
-        or die "Internal error: option '$long': unknown ${type}_argument";
+my %Getopt_Options;
+for my $cmd (sort keys %Command_Line_Options) {
+  LINE:
+    for my $line ( split "\n", $Command_Line_Options{$cmd} ) {
+        $line =~ m|^\s+\{\s* "(.*?)",\s* (.*?)_argument,\s* .*?,\s* '(.)'\s*\}|x
+            or next LINE;
+        my ( $long, $type, $short ) = ( $1, $2, $3 );
+        $type eq 'required' || $type eq 'no'
+            or die "Internal error: option '$long': unknown ${type}_argument";
 
-    my $x = "$long|$short";
-    $x .= ":s" if $type eq 'required';
+        my $x = "$long|$short";
+        $x .= ":s" if $type eq 'required';
 
-    push @Useradd_Opts, $x;
+        push @{ $Getopt_Options{$cmd} }, $x;
+    }
 }
 
-use Data::Dumper; print Dumper(\@Useradd_Opts);
+use Data::Dumper; print Dumper(\%Getopt_Options);
 
 # END   user-configurable section
 ###############################################################################
@@ -142,23 +161,32 @@ sub analyze {
             dprintf "%d : %s : %s\n", $line->lineno, $line->section, $cmd;
 
             $spec->context({ lineno => $lineno, excerpt => $cmd });
-            _check_add_command( $spec, $cmd );
+            _check_generic_add( $spec, $cmd );
         }
     }
 }
 
 
-sub _check_add_command {
+sub _check_generic_add {
     my $spec = shift;
     my $cmd  = shift;
 
-    my @words = quotewords('\s+', 0, $cmd); shift @words;
+    my @words = quotewords('\s+', 0, $cmd);
+
+    # useradd or groupadd
+    my $add_command = shift @words;
+    $add_command =~ s{^.*/}{};          # just the basename
+
+    my $getopt_options = $Getopt_Options{$add_command}
+        or die "$ME: Internal error: unknown add command '$add_command'";
+
+    # Parse the options
     local (@ARGV) = @words;
 #    use Data::Dumper; print Data::Dumper->Dump([ \@ARGV ], [ '@ARGV' ]);
 
-    my %args;
-    GetOptions( \%args, @Useradd_Opts);
-    use Data::Dumper; print Data::Dumper->Dump([ \%args ], [ '%args' ]);
+    my %options;
+    GetOptions( \%options, @{$getopt_options});
+    use Data::Dumper; print Data::Dumper->Dump([ \%options ], [ '%opts' ]);
 #    use Data::Dumper; print Data::Dumper->Dump([ \@ARGV ], [ '@ARGV' ]);
 
     my $arg = shift(@ARGV)
@@ -169,9 +197,22 @@ sub _check_add_command {
         };
     warn "$ME: WARNING: command '$cmd' left \@ARGV with '@ARGV'" if @ARGV;
 
-    # FIXME
-    # Home Directory : required
-    if (my $homedir = $args{'home-dir'}) {
+    {
+        no strict 'refs';
+        my $validator = "_check_${add_command}";
+        $validator->( $spec, \%options, $arg );
+    }
+}
+
+
+sub _check_useradd {
+    my $spec    = shift;
+    my $options = shift;
+    my $userid  = shift;
+
+   # Home Directory : required
+    if (my $homedir = $options->{'home-dir'}) {
+        # FIXME
     }
     else {
         $spec->gripe({
@@ -179,8 +220,43 @@ sub _check_add_command {
             diag => "Invocation of <tt>useradd</tt> without a home dir",
         });
     }
+
+    #
+    # Login shell
+    #
+    if (my $shell = $options->{shell}) {
+        if ($shell ne '/sbin/nologin') {
+            $spec->gripe({
+                code => 'UseraddBadShell',
+                diag => "Invocation of <tt>useradd</tt> with unexpected login shell <var>$shell</var> (expected <tt>/sbin/nologin</tt>)",
+            });
+        }
+    }
+    else {
+        $spec->gripe({
+            code => 'UseraddNoShell',
+            diag => "Invocation of <tt>useradd</tt> without a login shell",
+        });
+    }
+
+    #
+    # Userid
+    #
+    if (my $uid = $options->{uid}) {
+    }
+    else {
+        $spec->gripe({
+            code => 'UseraddNoUid',
+            diag => "Invocation of <tt>useradd</tt> without specifying a UID",
+        });
+    }
 }
 
+sub _check_groupadd {
+    my $spec    = shift;
+    my $options = shift;
+    my $groupid = shift;
+}
 
 1;
 
