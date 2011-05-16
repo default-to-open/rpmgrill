e44d7de3d640aafbf74f5693f541c0903e0091f0
Ed Santiago <santiago@redhat.com>
2011-05-16 13:12:22 -0600

    first try at sql schema for rpmgrill results

---
+++ sql/rpmgrill/001-initial-schema.sql
@@ -0,0 +1,111 @@
+DROP DATABASE IF EXISTS rpmgrill;
+
+CREATE DATABASE rpmgrill;
+USE rpmgrill;
+
+/*
+** Different versions of rpmgrill, brewtap, whatever we want to call it
+*/
+CREATE TABLE tools (
+       tool_id        INT AUTO_INCREMENT PRIMARY KEY,
+       tool_name      VARCHAR(256),
+       tool_version   VARCHAR(256)
+) TYPE InnoDB;
+
+/*
+** RHEL5, RHEL6, etc
+*/
+CREATE TABLE variants (
+       variant_id     INT AUTO_INCREMENT PRIMARY KEY,
+       variant_name   VARCHAR(256)
+) TYPE InnoDB;
+
+INSERT INTO variants (variant_id, variant_name) VALUES (0, 'Unknown');
+INSERT INTO variants (variant_id, variant_name) VALUES (3, 'RHEL3');
+INSERT INTO variants (variant_id, variant_name) VALUES (4, 'RHEL4');
+INSERT INTO variants (variant_id, variant_name) VALUES (5, 'RHEL5');
+INSERT INTO variants (variant_id, variant_name) VALUES (6, 'RHEL6');
+INSERT INTO variants (variant_id, variant_name) VALUES (7, 'RHEL7');
+INSERT INTO variants (variant_id, variant_name) VALUES (8, 'RHEL8');
+
+/*
+** Each row here is an N-V-R; the analyzed_by and _when fields may be used
+** to identify which version of brewtap ran the analysis and when. This
+** may be important to identify builds that need to be re-analyzed.
+*/
+CREATE TABLE runs (
+       run_id           INT AUTO_INCREMENT PRIMARY KEY,
+       analyzed_when    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       analyzed_by      INT NOT NULL,
+
+       package_name     VARCHAR(256) NOT NULL,
+       package_version  VARCHAR(256) NOT NULL,
+       package_release  VARCHAR(256) NOT NULL,
+       variant_id       INT NOT NULL,
+       brew_id          INT NOT NULL,           /* FIXME */
+
+       /* FIXME: Do we really need failed? */
+       status           ENUM('pending','running','completed','failed') NOT NULL,
+
+       INDEX (package_name),
+       INDEX (package_name, package_version, package_release),
+
+       FOREIGN KEY        (analyzed_by)
+         REFERENCES tools (tool_id)
+         ON DELETE RESTRICT,
+
+       FOREIGN KEY           (variant_id)
+         REFERENCES variants (variant_id)
+         ON DELETE RESTRICT
+) TYPE InnoDB;
+
+CREATE TABLE plugins (
+       plugin_id     INT AUTO_INCREMENT PRIMARY KEY,
+       plugin_name   VARCHAR(256) NOT NULL
+) TYPE InnoDB;
+
+CREATE TABLE codes (
+       code_id       INT AUTO_INCREMENT PRIMARY KEY,
+       code_name     VARCHAR(256) NOT NULL
+) TYPE InnoDB;
+
+CREATE TABLE tests_run (
+       tests_run_id    INT AUTO_INCREMENT PRIMARY KEY,
+       run_id          INT NOT NULL,
+       plugin_id       INT NOT NULL,
+       test_order      INT NOT NULL,
+       test_status     ENUM ('NOTRUN', 'completed', 'failed'),
+       fail_message    VARCHAR(256),
+
+       FOREIGN KEY       (run_id)
+         REFERENCES runs (run_id)
+         ON DELETE RESTRICT,
+       FOREIGN KEY          (plugin_id)
+         REFERENCES plugins (plugin_id)
+         ON DELETE RESTRICT
+) TYPE InnoDB;
+
+CREATE TABLE results (
+       result_id     INT AUTO_INCREMENT PRIMARY KEY,
+       tests_run_id  INT NOT NULL,
+       code_id       INT,
+
+       diagnostic    VARCHAR(256) NOT NULL,     /* FIXME: maybe TEXT? */
+       excerpt       VARCHAR(256),              /* FIXME */
+       context_path     VARCHAR(256),
+       context_lineno   VARCHAR(256),
+       context_subcontext   VARCHAR(256),
+
+       FOREIGN KEY            (tests_run_id)
+         REFERENCES tests_run (tests_run_id)
+         ON DELETE RESTRICT
+) TYPE InnoDB;
+
+CREATE TABLE schema_version (
+       applied_ts           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       migration_code       VARCHAR(128) PRIMARY KEY,
+       description          VARCHAR(256)
+) TYPE InnoDB;
+
+INSERT INTO schema_version (migration_code, description)
+       VALUES ('001', 'Initial database creation');
